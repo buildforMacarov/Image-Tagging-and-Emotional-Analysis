9d17fdd6400a2f68c2d18696fc1166ed801df270
Ritik <ritiksinha005@gmail.com>
2019-02-12 17:06:51 +0530

    collecting data

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -5,3 +5,12 @@ class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['http://brickset.com/sets/year-2016']
     
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        for brickset in response.css(SET_SELECTOR):
+            pass
+    
+        NAME_SELECTOR = 'h1 a ::text'
+        yield {
+            'name': brickset.css(NAME_SELECTOR).extract_first(),
+        }
